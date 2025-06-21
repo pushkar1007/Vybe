7142e150ebbab2c78ba303c2dcd344d8c7a8c09f
pushkar1007 <nanwanipushkar@gmail.com>
2025-06-22 00:25:23 +0530

    given config value to ChakraProvider as prop and created LogoIcon component

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ChakraProvider } from "@chakra-ui/react";
+import system from "./theme/config";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ChakraProvider>
+    <ChakraProvider value={system}>
       <App />
     </ChakraProvider>
   </StrictMode>,
