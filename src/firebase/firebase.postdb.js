13442549d4c9424375d0d399fdf88184954a3ae1
Aayush <chandayush005@gmail.com>
2025-06-27 11:45:43 +0530

    post database added and fixed userdb

---
+++ src/firebase/firebase.postdb.js
@@ -0,0 +1,151 @@
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  updateDoc,
+  arrayUnion,
+  arrayRemove,
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+} from "firebase/firestore";
+import { firebaseConfig } from "./config.js";
+
+class Firebase {
+  app;
+  db;
+
+  constructor() {
+    this.app = initializeApp(firebaseConfig);
+    this.db = getFirestore(app);
+  }
+
+  async createPost({ content, image }, currentUser) {
+    try {
+      const postRef = await addDoc(collection(this.db, "posts"), {
+        content,
+        image: image ? image : "",
+        likes: [],
+        comments: [],
+        createdBy: currentUser.uid,
+        createdAt: String(Date.now()),
+        updatedAt: null,
+      });
+      const post = await updateDoc(postRef, { postId: postRef.id });
+      return post;
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error("Error creating post:", errorCode, errorMessage);
+    }
+  }
+
+  async updatePost({ content, image, postId }) {
+    try {
+      const postRef = doc(this.db, "posts", postId);
+
+      content
+        ? await updateDoc(postRef, {
+            content,
+          })
+        : null;
+
+      image
+        ? await updateDoc(postRef, {
+            image,
+          })
+        : null;
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error("Error updating post:", errorCode, errorMessage);
+    }
+  }
+
+  async deletePost(postId) {
+    try {
+      const postRef = doc(this.db, "posts", postId);
+      await deleteDoc(postRef);
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error("Error deleting post:", errorCode, errorMessage);
+    }
+  }
+
+  async getPostData(postId) {
+    try {
+      const postRef = doc(this.db, "posts", postId);
+      const snap = await getDoc(postRef);
+      if (snap.exists()) {
+        return { id: snap.id, ...snap.data };
+      } else {
+        return null;
+      }
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error("Error getting  post data:", errorCode, errorMessage);
+    }
+  }
+
+  async likePost(postId, userId) {
+    try {
+      const postRef = doc(this.db, "posts", postId);
+      const userRef = doc(this.db,"users",userId)
+      await updateDoc(postRef, {
+        likes: arrayUnion(userRef),
+      });
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error("Error liking  post :", errorCode, errorMessage);
+    }
+  }
+
+  async unlikePost(postId, userId) {
+    try {
+      const postRef = doc(this.db, "posts", postId);
+      const userRef = doc(this.db,"users",userId)
+      await updateDoc(postRef, {
+        likes: arrayRemove(userRef),
+      });
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error("Error unliking  post :", errorCode, errorMessage);
+    }
+  }
+
+  async addComment(postId, commentId) {
+    try {
+      const postRef = doc(this.db, "posts", postId);
+      const commentRef = doc(this.db,"comments",commentId)
+      await updateDoc(postRef, {
+        comments: arrayUnion(commentRef),
+      });
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error("Error adding comment to  post :", errorCode, errorMessage);
+    }
+  }
+
+  async removeComment(postId, commentId) {
+    try {
+      const postRef = doc(this.db, "posts", postId);
+      const commentRef = doc(this.db,"comments",commentId)
+      await updateDoc(postRef, {
+        comments: arrayUnion(commentRef),
+      });
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error("Error removing comment from  post :", errorCode, errorMessage);
+    }
+  }
+  
+}
+
+export default new Firebase();
